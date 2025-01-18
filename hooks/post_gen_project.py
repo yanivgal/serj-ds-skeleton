7b2fb98b42b9145cf1091b5c6a6ebedbbd50c694
yanivgal <yanivgal123@gmail.com>
2025-01-18 10:43:34 +0200

    refactor: rename .placeholder files to .gitkeep

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,10 @@
 import os
 
 def remove_placeholders():
-    """Recursively removes .placeholder files from the generated project."""
+    """Recursively removes .gitkeep placeholder files from the generated project."""
     for root, dirs, files in os.walk("."):
         for file in files:
-            if file == ".placeholder":
+            if file == ".gitkeep":
                 os.remove(os.path.join(root, file))
 
 if __name__ == "__main__":
